f8f754e054d9166452201c010bd393c4ee4399de
yeti <yetnet.behailu@hotmail.com>
2024-03-30 20:34:41 +0100

    Installed React Router to handle navigation & added a navbar menu with basic styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import Navbar from './components/Navbar';
 import MovieList from './components/movies/MovieList';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* Render the MovieList component */}
-      <MovieList />
-    </div>
+    <Router> {/* BrowserRouter */}
+      <>
+        {/* Render Navbar component */}
+        <Navbar />
+        {/* Render the MovieList component */}
+        <MovieList />
+      </>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return(
+        <nav className='navbar navbar-expand-lg bg-nav mb-3'>
+            <div className='container'>
+                <Link className='navbar-brand' to="/">MovieMart</Link>
+                <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' 
+                    aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        {/* Use Link component instead of <a> for navigation */}
+                        <Link className="nav-link active" to="/movies">Movies</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+export default Navbar;
