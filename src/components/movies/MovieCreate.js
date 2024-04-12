837dae13e03e8760028637774ee88be40d772427
yeti <yetnet.behailu@hotmail.com>
2024-04-12 07:56:40 +0200

    Update: Display toast message upon failure to create movie

--- src/components/movies/MovieCreate.js
+++ src/components/movies/MovieCreate.js
@@ -71,70 +71,76 @@ const MovieCreate = () => {
                 // Redirect to MovieList page with success message as query parameter
                 navigate('/movies?success=Movie created successfully!');
             } else {
-                setErrorMessage('Failed to create movie. Please try again.');
+                setErrorMessage('Failed to create movie. Please try again!');
             }
         } catch (error) {
             // Handle network errors or other exceptions
-            setErrorMessage('An error occurred while creating the movie.');
+            setErrorMessage('An error occurred while creating the movie');
         }
 
     };
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-10 offset-1 col-md-8 offset-md-2 movie-form">
-                    <h1 className="form-title">Create a new movie</h1>
-                    <form onSubmit={handleSubmit} className="form-content">
-                        <div className="mb-3 mt-3 d-flex">
-                            <label htmlFor="title" className="form-label">Title</label>
-                            <input type="text" className="form-control" id="title" name="title" value={movieData.title} onChange={handleChange} />
-                        </div>
-                        <div className="mb-3 d-flex">
-                            <label htmlFor="genre" className="form-label">Genre</label>
-                            <input type="text" className="form-control" id="genre" name="genre" value={extractName(movieData.genre)} onChange={handleChange}/>
-                        </div>
-                        <div className="mb-3 d-flex">
-                            <label htmlFor="director" className="form-label">Director</label>
-                            <input type="text" className="form-control" id="director" name="director" value={extractName(movieData.director)} onChange={handleChange}/>
-                        </div>
-                        <div className="mb-3 d-flex">
-                            <label htmlFor="description" className="form-label">Description</label>
-                            <textarea className="form-control textarea" id="description" name="description" value={movieData.description} onChange={handleChange}/>
-                        </div>
-                        <div className='row justify-content-center'>
-                            <div className="col-md-6 mb-3 d-flex justify-content-center justify-content-md-start language-content">
-                                <label htmlFor="language" className="form-label">Language</label>
-                                <input type="text" className="form-control" id="language" name="language" value={movieData.language} onChange={handleChange}/>
+        <>
+            {errorMessage && (
+                <div className="alert alert-danger toast-error" role="alert">
+                    {errorMessage}
+                </div>
+            )}
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 offset-1 col-md-8 offset-md-2 movie-form">
+                        <h1 className="form-title">Create a new movie</h1>
+                        <form onSubmit={handleSubmit} className="form-content" >
+                            <div className="mb-3 mt-3 d-flex">
+                                <label htmlFor="title" className="form-label">Title</label>
+                                <input type="text" className="form-control" id="title" name="title" value={movieData.title} onChange={handleChange} />
                             </div>
-                            <div className="col-md-6 mb-3 d-flex justify-content-center justify-content-md-start duration-content">
-                                <label htmlFor="duration" className="form-label">Duration</label>
-                                <input type="text" className="form-control" id="duration" name="duration" value={movieData.duration} onChange={handleChange} placeholder="HH:MM:SS"/>
+                            <div className="mb-3 d-flex">
+                                <label htmlFor="genre" className="form-label">Genre</label>
+                                <input type="text" className="form-control" id="genre" name="genre" value={extractName(movieData.genre)} onChange={handleChange}/>
                             </div>
-                        </div>
-                        <div className='mb-3 d-flex'>
-                            <label htmlFor='actors' className='form-label'>Actors</label>
-                            <textarea className='form-control' id="actors" name="actors" placeholder='Separate names with commas(,)' value={movieData.actors.map(actor => actor.Name).join(',')} onChange={handleChange}/>
-                        </div>
-                        <div className='row justify-content-center'>
-                            <div className="col-md-6 mb-3 d-flex justify-content-center justify-content-md-start image-content">
-                                <label htmlFor="imageUrl" className="form-label">Image</label>
-                                <input type="text" className="form-control" id="imageUrl" placeholder="http://...." name="imageUrl" value={movieData.imageUrl} onChange={handleChange} />
+                            <div className="mb-3 d-flex">
+                                <label htmlFor="director" className="form-label">Director</label>
+                                <input type="text" className="form-control" id="director" name="director" value={extractName(movieData.director)} onChange={handleChange}/>
                             </div>
-                            <div className="col-md-6 mb-3 d-flex justify-content-center justify-content-md-start date-content">
-                                <label htmlFor="releaseDate" className="form-label">Rel. date</label>
-                                <input type="date" className="form-control" id="releaseDate" 
-                                name="releaseDate" value={movieData.releaseDate} onChange={handleChange} />
+                            <div className="mb-3 d-flex">
+                                <label htmlFor="description" className="form-label">Description</label>
+                                <textarea className="form-control textarea" id="description" name="description" value={movieData.description} onChange={handleChange}/>
                             </div>
-                        </div>
-                        <div className="text-center">
-                            <button type="submit" className="btn form-create-btn">Enter</button>
-                        </div>
-                        {errorMessage && <div className="error-message">{errorMessage}</div>}
-                    </form>
+                            <div className='row justify-content-center'>
+                                <div className="col-md-6 mb-3 d-flex justify-content-center justify-content-md-start language-content">
+                                    <label htmlFor="language" className="form-label">Language</label>
+                                    <input type="text" className="form-control" id="language" name="language" value={movieData.language} onChange={handleChange}/>
+                                </div>
+                                <div className="col-md-6 mb-3 d-flex justify-content-center justify-content-md-start duration-content">
+                                    <label htmlFor="duration" className="form-label">Duration</label>
+                                    <input type="text" className="form-control" id="duration" name="duration" value={movieData.duration} onChange={handleChange} placeholder="HH:MM:SS"/>
+                                </div>
+                            </div>
+                            <div className='mb-3 d-flex'>
+                                <label htmlFor='actors' className='form-label'>Actors</label>
+                                <textarea className='form-control' id="actors" name="actors" placeholder='Separate names with commas(,)' value={movieData.actors.map(actor => actor.Name).join(',')} onChange={handleChange}/>
+                            </div>
+                            <div className='row justify-content-center'>
+                                <div className="col-md-6 mb-3 d-flex justify-content-center justify-content-md-start image-content">
+                                    <label htmlFor="imageUrl" className="form-label">Image</label>
+                                    <input type="text" className="form-control" id="imageUrl" placeholder="http://...." name="imageUrl" value={movieData.imageUrl} onChange={handleChange} />
+                                </div>
+                                <div className="col-md-6 mb-3 d-flex justify-content-center justify-content-md-start date-content">
+                                    <label htmlFor="releaseDate" className="form-label">Rel. date</label>
+                                    <input type="date" className="form-control" id="releaseDate" 
+                                    name="releaseDate" value={movieData.releaseDate} onChange={handleChange} />
+                                </div>
+                            </div>
+                            <div className="text-center">
+                                <button type="submit" className="btn form-create-btn">Enter</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
